dad16527183c8f637e5ec58ab0d2fd0a7136443a
jfd02 <jerrydurkin02@gmail.com>
2026-03-22 17:54:01 -0700

    Bump generated Package.swift to swift-tools-version:6.0
    Generated clients depend on this repo which requires 6.0.

--- Sources/CreateAPI/Generator/Generator+Package.swift
+++ Sources/CreateAPI/Generator/Generator+Package.swift
@@ -27,7 +27,7 @@ extension Generator {
             .joined(separator: ",\n")
 
         return """
-        // swift-tools-version:5.5
+        // swift-tools-version:6.0
         // The swift-tools-version declares the minimum version of Swift required to build this package.
 
         import PackageDescription
